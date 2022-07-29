e4b6c8bba19366c6b1f847eabf36772a48759b72
Wallyson <wallysom2@gmail.com>
2022-07-29 13:01:53 -0300

    carregando lista de filmes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Header from './Header';
 
 export default function MovieList () {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+		const request = axios.get("https://mock-api.driven.com.br/api/v7/cineflex/movies");
+        request.then(response => {
+            setMovies(response.data);
+        });
+	}, []);
+
     return (
         <MovieListStyled>
              <Header /> 
            <h2>
                 Selecione o filme
            </h2>
-           
+              <ul>
+                {movies.map(movie => (
+                    <li key={movie.id}>
+                            <img src={movie.posterURL} alt={movie.title} />
+                    </li>
+                ))}
+            </ul>
 
         </MovieListStyled>
     );
@@ -34,6 +51,31 @@ const MovieListStyled = styled.div`
         align-items: center;
         text-align: center;
         color: #293845;
+    }
+    ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        li {
+            width: 50%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            img {
+                width: 129px;
+                height: 193px;
+                object-fit: cover;
+            }
+        }
     }
 `;
 
